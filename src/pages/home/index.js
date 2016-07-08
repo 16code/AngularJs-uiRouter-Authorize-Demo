b837f0ae49581a2a62147ca69a20fae9273745cb
Liuxin <130866602@qq.com>
2016-07-08 18:27:40 +0800

    rename folder name

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,6 @@
 import './home.less';
 import HomeController from './home.controller';
-import hero from '../../components/hero-box';
+import hero from '../../components/hero';
 import learnMore from '../../components/learn-more';
 const module = angular.module('app.pages.home', [
     hero.name,
