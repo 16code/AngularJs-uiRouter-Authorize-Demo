12ff6259f961a87fd844c5092cc6b65f0702b111
Liuxin <130866602@qq.com>
2016-07-08 17:48:32 +0800

    page update

--- src/pages/home/home.controller.js
+++ src/pages/home/home.controller.js
@@ -5,6 +5,20 @@ class HomeController {
             subject: 'Webpack + Angular + UiRouter + ES6最佳实践!',
             intro: '简单、高效、快捷的构建你的Angular应用程序'
         };
+        this.ads = [
+            require('../../assets/images/ad-1.jpg'),
+            require('../../assets/images/ad-2.jpg')
+        ];
+        this.products = [
+            require('../../assets/images/1.jpg'),
+            require('../../assets/images/2.jpg'),
+            require('../../assets/images/3.jpg'),
+            require('../../assets/images/4.jpg'),
+            require('../../assets/images/5.jpg'),
+            require('../../assets/images/6.jpg'),
+            require('../../assets/images/7.jpg'),
+            require('../../assets/images/8.jpg')
+        ];
         this.categories = [
             {
                 name: '包包',
