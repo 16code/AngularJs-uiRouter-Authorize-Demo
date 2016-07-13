78a752e9271e54165733156656e3428af15e2343
Liuxin <130866602@qq.com>
2016-07-13 13:12:41 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
-const path = require('path'),
-    args = require('yargs').argv,
-    autoprefixer = require('autoprefixer'),
+const path = require('path');
+const args = require('yargs').argv;
+const autoprefixer = require('autoprefixer');
     // webpack
-    webpack = require('webpack'),
-    ExtractTextPlugin = require('extract-text-webpack-plugin'),
-    HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // get shell parameters
-const isProduct = args.prod,
-    isMock = args.mock,
-    APP_NAME = JSON.stringify('App');
-
+const isProduct = args.prod;
+const isMock = args.mock;
+const APP_NAME = JSON.stringify('App');
+const API_HOST = JSON.stringify('//api.themoviedb.org/3');
+const IMAGE_URI = JSON.stringify('//image.tmdb.org/t/p');
+const MOIVES_API_KEY = JSON.stringify('7b743a8f4b20e473e2d8bf71fdc313e4');
 // webpack plugin config
 const webpackPluginCommonConfig = [
     // 声明一些全局的变量 可以在任何js以及模板引擎文件中判断当前环境 example: if(__PROD__);
     new webpack.DefinePlugin({
         __PROD__: isProduct,
         __MOCK__: isMock,
-        __APP_NAME__: APP_NAME
+        __APP_NAME__: APP_NAME,
+        __API_HOST__: API_HOST,
+        __IMAGE_URI__: IMAGE_URI,
+        __MOIVES_API_KEY__: MOIVES_API_KEY
     }),
     // 抽离公共依赖打包进 vendor.js
     new webpack.optimize.CommonsChunkPlugin({
@@ -78,44 +82,41 @@ const webpackConfig = {
         alias: {
             angular: nodeModulesPath('/angular/index'),
             'angular-ui-router': nodeModulesPath('/angular-ui-router/release/angular-ui-router.min'),
-            oclazyload: nodeModulesPath('/oclazyload/dist/ocLazyLoad.min')
+            oclazyload: nodeModulesPath('/oclazyload/dist/ocLazyLoad.min'),
+            'angular-resource': nodeModulesPath('/angular-resource/index')
         },
         modulesDirectories: ['bower_components', 'node_modules'],
         extensions: ['', '.webpack.js', '.web.js', '.js', '.ejs', '.css', '.less', '.png', '.jpg', '.gif', '.svg']
     },
     module: {
-        preLoaders: [
-            {
-                test: /\.js$/,
-                loader: 'eslint',
-                exclude: /node_modules/
-            }
-        ],
-        loaders: [
-            {
-                test: /\.js$/,
-                loader: 'ng-annotate?add=true!babel',
-                exclude: /(node_modules|src\/libs)/
-            }, {
-                test: /\.json$/,
-                loader: 'json-loader'
-            }, {
-                test: /\.html$/,
-                loader: 'html-loader?attrs=img:src img:data-src'
-            }, {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
-            }, {
-                test: /\.less$/,
-                loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
-            }, {
-                test: /\.(woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
-                loader: 'url-loader?limit=1&name=/assets/fonts/[name].[ext]?[hash]'
-            }, {
-                test: /\.(png|jpg|gif)$/,
-                loader: 'file-loader?limit=8192&name=/assets/images/[name].[hash].[ext]'
-            }
-        ],
+        preLoaders: [{
+            test: /\.js$/,
+            loader: 'eslint',
+            exclude: /node_modules/
+        }],
+        loaders: [{
+            test: /\.js$/,
+            loader: 'ng-annotate?add=true!babel',
+            exclude: /(node_modules|src\/libs)/
+        }, {
+            test: /\.json$/,
+            loader: 'json-loader'
+        }, {
+            test: /\.html$/,
+            loader: 'html-loader?attrs=img:src img:data-src'
+        }, {
+            test: /\.css$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+        }, {
+            test: /\.less$/,
+            loader: ExtractTextPlugin.extract('style-loader', 'css-loader!less-loader')
+        }, {
+            test: /\.(woff|woff2|ttf|eot|svg)(\?]?.*)?$/,
+            loader: 'url-loader?limit=1&name=/assets/fonts/[name].[ext]?[hash]'
+        }, {
+            test: /\.(png|jpg|gif)$/,
+            loader: 'file-loader?limit=8192&name=/assets/images/[name].[hash].[ext]'
+        }],
         htmlLoader: {
             ignoreCustomFragments: [/\{\{.*?}}/]
         }
@@ -135,10 +136,11 @@ const webpackConfig = {
         host: '127.0.0.1',
         port: 8080
     },
-    postcss: function() {
+    postcss() {
         return [autoprefixer];
     }
 };
+
 function nodeModulesPath(filePath) {
     return path.join(__dirname, 'node_modules', filePath);
 }
